718daa8c14504fccd4362bd842facb1721eb9b4f
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-09-02 14:45:43 -0700

    feat:server set-up

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const port = process.env.PORT || 3002;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
