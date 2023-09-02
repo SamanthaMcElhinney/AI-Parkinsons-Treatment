3db5a1f0b5844fff837e0aff76eb5c3008714324
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-09-02 14:02:20 -0700

    feat: form for search generation/ client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./styles.module.css";
+import speaking from "./assets/speaking.png";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [wordDescription, setWordDescription] = useState("")
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted:", wordDescription)
+    return
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.main}>
+      <img src={speaking} alt="" className="styles.icon" />
+      <h3>Generate search with AI</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="describe word"
+          onChange={(e)=> {
+            setWordDescription(e.target.value)
+          }}
+        />
+        <input 
+          type="submit"
+          value="describe your word"
+          />
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;
